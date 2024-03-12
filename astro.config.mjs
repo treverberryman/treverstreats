628bd74e3918fb554e1a1a068fd8bc029c062521
treverberryman <trever.berryman@gmail.com>
2024-03-12 14:50:27 -0400

    Added Google Analytics, Party Town Astro Extension, and a new blog post for Creative Tech with Jill Shah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   base: '/',
-  integrations: [mdx()]
+  integrations: [mdx(), partytown({ config: { forward: ['dataLayer.push'] } })]
 });

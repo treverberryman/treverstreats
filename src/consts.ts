10215bdffff94a14703e2e5e59a32dd0e9e18fc4
treverberryman <trever.berryman@gmail.com>
2025-07-17 09:11:16 -0400

    initial push to github

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Trever Berryman | Personal Website';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Trever's Treats';
+export const SITE_DESCRIPTION = 'A modern cooking website featuring recipes, how-tos, gear reviews, and a personal blog. Designed for food lovers, home cooks, and anyone interested in creative technology in the kitchen.';
